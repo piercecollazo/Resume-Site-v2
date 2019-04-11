34566a5c9db50f30202197a33211af4e8d74247e
Pierce Collazo <pierce.collazo@codeimmersive.com>
2019-04-11 16:23:23 -0400

    added a resume container, alternating between home and the resume list on the same page.

--- main.js
+++ main.js
@@ -1,19 +1,61 @@
 window.onload = init;
 
 function init(){
-  // document.querySelector('#resume-button')
-  //   .addEventListener('click', );
+  document.querySelector('#home-button')
+    .addEventListener('click', displayHome)
+  document.querySelector('#resume-button')
+    .addEventListener('click', displayResume);
 
   document.querySelector('#contact-submit')
     .addEventListener('click', contact);
 
 }
 
+function displayHome(event){
+  event.preventDefault();
+  clearResume();
+  document.querySelector('#content').className = 'container-fluid'
+  document.querySelector('#resume-page').className = 'container-fluid d-none'
+}
+
 function displayResume(event){
   event.preventDefault();
-  for(let i = 0; i < resume.length; i++){
-
+  document.querySelector('#content').className = 'container-fluid d-none'
+  document.querySelector('#resume-page').className = 'container-fluid'
+  const mother = document.querySelector('#resume-mother');
+  if(mother.hasChildNodes()){
+    clearResume();
+    for(let i = 0; i < resume.length; i++){
+      resumeList(i);      
+    }
+  }else{
+    for(let i = 0; i < resume.length; i++){
+      resumeList(i);      
+    }
   }
+}
+
+function resumeList(i){
+  // Basic code for making a list within a list
+  let mother = document.querySelector('#resume-mother');
+  let newLine = document.createElement('li');
+  let newList = document.createElement('ul');
+    newList.className = 'child-list'
+
+  // code for the lines of child list for the resume
+  let nameLine = document.createElement('li')
+        nameLine.innerText = resume[i].company;
+  let posLine = document.createElement('li')
+        posLine.innerText =resume[i].position;
+  let timeLine = document.createElement('li')
+        timeLine.innerText =resume[i].time;
+
+  mother.appendChild(newLine);
+  newLine.appendChild(newList);
+  newList.appendChild(nameLine);
+  newList.appendChild(posLine);
+  newList.appendChild(timeLine);
+  
 }
 
 function contact(event){
@@ -28,3 +70,9 @@ function contact(event){
 
 }
 
+function clearResume(){
+  let mother = document.querySelector('#resume-mother');
+  while(mother.hasChildNodes()){
+    mother.removeChild(mother.firstChild)
+  }
+}
